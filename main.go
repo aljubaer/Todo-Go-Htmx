3ca05cd4708109e6f7a1d9c6c6c18c0bd226f555
aljubaer <bsse0812@iit.du.ac.bd>
2024-04-03 02:51:35 +0600

    init project

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, _ := template.New("").ParseFiles("templates/index.html")
+		tmpl.ExecuteTemplate(w, "Base", nil)
+	})
+
+	http.ListenAndServe(":3000", r)
+}
